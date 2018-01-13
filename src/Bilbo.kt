a74aaad0ee43ac3617799ac3265544412b2ef317
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2018-01-13 14:40:41 -0200

    camelCasing

--- src/Bilbo.kt
+++ src/Bilbo.kt
@@ -1,31 +1,31 @@
-class Bilbo(var ntree: Int, var mattributes: Int, var instances: MutableList<Instance>, var categoricalAttributesValues: Map<Int, List<Double>>) {
-    val bootstrap_sets: MutableList<Bootstrap> = mutableListOf()
-    val decision_trees: MutableList<DecisionTree> = mutableListOf()
+class Bilbo(var nTree: Int, var mAttributes: Int, var instances: MutableList<Instance>, var categoricalAttributesValues: Map<Int, List<Double>>) {
+    val bootstrapSets: MutableList<Bootstrap> = mutableListOf()
+    val decisionTrees: MutableList<DecisionTree> = mutableListOf()
 
     init {
         generateBootstraps()
-        println(bootstrap_sets[2].bootstrapSet.size)
+        println(bootstrapSets[2].bootstrapSet.size)
         generateDecisionTrees()
     }
 
     private fun generateDecisionTrees() {
-        var planter = Planter(instances, categoricalAttributesValues, mattributes)
-        for (number_of_sets in 1..ntree) {
-            decision_trees.add(planter.plantTree())
+        val planter = Planter(instances, categoricalAttributesValues, mAttributes)
+        for (number_of_sets in 1..nTree) {
+            decisionTrees.add(planter.plantTree())
         }
     }
 
     private fun generateBootstraps() {
-        for (number_of_sets in 1..ntree) {
-            bootstrap_sets.add(Bootstrap(instances))
+        for (number_of_sets in 1..nTree) {
+            bootstrapSets.add(Bootstrap(instances))
         }
     }
 
     public fun testTrees() {
         val answers = mutableListOf<Int>()
         (0..instances.size).forEach {
-            i -> decision_trees.forEachIndexed {
-                index, decisionTree -> answers.add(decisionTree.decide(bootstrap_sets[index].instances[i]))
+            i -> decisionTrees.forEachIndexed {
+                index, decisionTree -> answers.add(decisionTree.decide(bootstrapSets[index].instances[i]))
             }
             println(answers.voteDecision())
         }
@@ -46,11 +46,11 @@ class Bilbo(var ntree: Int, var mattributes: Int, var instances: MutableList<Ins
 fun main(args: Array<String>) {
     val dataReader = DataReader("./data/haberman.data")
     val bilbo = Bilbo(10, 3, dataReader.trainingDataSet, dataReader.categoricalAttributesValues)
-    println(bilbo.bootstrap_sets[0].bootstrapSet)
-    println(bilbo.bootstrap_sets[1].bootstrapSet)
-    println(bilbo.bootstrap_sets[2].bootstrapSet)
-    println(bilbo.bootstrap_sets[2].bootstrapSet.size)
-    println(bilbo.decision_trees)
+    println(bilbo.bootstrapSets[0].bootstrapSet)
+    println(bilbo.bootstrapSets[1].bootstrapSet)
+    println(bilbo.bootstrapSets[2].bootstrapSet)
+    println(bilbo.bootstrapSets[2].bootstrapSet.size)
+    println(bilbo.decisionTrees)
     println(bilbo.testTrees()
     )
     println("ok")
